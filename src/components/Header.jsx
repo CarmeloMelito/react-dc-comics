976f64f6627fb5b280eb88125c7fffa30739fe28
CarmeloMelito <melitocarmelo211@gmail.com>
2025-04-04 10:40:56 +0200

    feat:menu header css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,19 @@
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header>
+      <ul className="header-menu">
+        <li>Characters</li>
+        <li>Comics</li>
+        <li>Movies</li>
+        <li>Tv</li>
+        <li>Games</li>
+        <li>Collectibles</li>
+        <li>Videos</li>
+        <li>Fans</li>
+        <li>News</li>
+        <li>Shop</li>
+      </ul>
+    </header>
+  );
 }
 export default Header;
