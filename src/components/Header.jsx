e57e9b88b61cbe35a64535d50afd23a12e7afaa7
CarmeloMelito <melitocarmelo211@gmail.com>
2025-04-04 10:55:10 +0200

    feat:main content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,36 @@ function Header() {
   return (
     <header>
       <ul className="header-menu">
-        <li>Characters</li>
-        <li>Comics</li>
-        <li>Movies</li>
-        <li>Tv</li>
-        <li>Games</li>
-        <li>Collectibles</li>
-        <li>Videos</li>
-        <li>Fans</li>
-        <li>News</li>
-        <li>Shop</li>
+        <li>
+          <a href="">Characters</a>
+        </li>
+        <li>
+          <a href="">Comics</a>
+        </li>
+        <li>
+          <a href="">Movies</a>
+        </li>
+        <li>
+          <a href="">Tv</a>
+        </li>
+        <li>
+          <a href="">Games</a>
+        </li>
+        <li>
+          <a href="">Collectibles</a>
+        </li>
+        <li>
+          <a href="">Videos</a>
+        </li>
+        <li>
+          <a href="">Fans</a>
+        </li>
+        <li>
+          <a href="">News</a>
+        </li>
+        <li>
+          <a href="">Shop</a>
+        </li>
       </ul>
     </header>
   );
