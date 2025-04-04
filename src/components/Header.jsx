b222cd329d180e38b8385eade894dd662f312da3
CarmeloMelito <melitocarmelo211@gmail.com>
2025-04-04 11:59:01 +0200

    feat:add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,41 @@
+import logo from "../assets/img/dc-logo.png";
+
 function Header() {
   return (
     <header>
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
       <ul className="header-menu">
         <li>
-          <a href="">Characters</a>
+          <a href="#">Characters</a>
         </li>
         <li>
-          <a href="">Comics</a>
+          <a href="#">Comics</a>
         </li>
         <li>
-          <a href="">Movies</a>
+          <a href="#">Movies</a>
         </li>
         <li>
-          <a href="">Tv</a>
+          <a href="#">Tv</a>
         </li>
         <li>
-          <a href="">Games</a>
+          <a href="#">Games</a>
         </li>
         <li>
-          <a href="">Collectibles</a>
+          <a href="#">Collectibles</a>
         </li>
         <li>
-          <a href="">Videos</a>
+          <a href="#">Videos</a>
         </li>
         <li>
-          <a href="">Fans</a>
+          <a href="#">Fans</a>
         </li>
         <li>
-          <a href="">News</a>
+          <a href="#">News</a>
         </li>
         <li>
-          <a href="">Shop</a>
+          <a href="#">Shop</a>
         </li>
       </ul>
     </header>
