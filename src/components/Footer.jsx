e0b5273d75fcc565fbfb41686509d27078d64e17
CarmeloMelito <melitocarmelo211@gmail.com>
2025-04-06 20:32:51 +0200

    add:list footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,59 @@
 function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div className="footerList">
+        <div>
+          <div>
+            <h3>Dc Comics</h3>
+            <ul>
+              <li>Carachters</li>
+              <li>Comics</li>
+              <li>Movies</li>
+              <li>Tv</li>
+              <li>Games</li>
+              <li>Videos</li>
+              <li>News</li>
+            </ul>
+            <h3>Shop</h3>
+            <ul>
+              <li>Shop Dc</li>
+              <li>Shop DC Collectibles</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h3>Dc</h3>
+            <ul>
+              <li>Terms of Use</li>
+              <li>Privacy policy (New)</li>
+              <li>Ad Choices</li>
+              <li>Advertising</li>
+              <li>Jobs</li>
+              <li>Subscription</li>
+              <li>Talent Workshop</li>
+              <li>CPSC Certificates</li>
+              <li>Ratings</li>
+              <li>Shop Help</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h3>Sites</h3>
+            <ul>
+              <li>DC</li>
+              <li>MAD Magazines</li>
+              <li>DC Kids</li>
+              <li>DC Universe</li>
+              <li>DC Power Visa</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
