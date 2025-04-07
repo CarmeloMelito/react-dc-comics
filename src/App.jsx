8064aa6c3c30487848a1b8b39d802a28763bb102
CarmeloMelito <melitocarmelo211@gmail.com>
2025-04-07 11:46:23 +0200

    feat:create footerLinks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import FooterLink from "./components/footerLink";
+import FooterLink from "./components/FooterLink";
 function App() {
   return (
     <>
--- src/components/FooterLink.jsx
+++ src/components/FooterLink.jsx
@@ -1,4 +1,32 @@
+import FacebookImg from "../assets/img/facebookimg.png";
+import TwitterImg from "../assets/img/footer-twitter.png";
+import YoutubeImg from "../assets/img/footer-youtube.png";
+import PinterestImg from "../assets/img/footer-pinterest.png";
+import PeriscopeImg from "../assets/img/footer-periscope.png";
+
 function FooterLink() {
-  return <div>linkl</div>;
+  return (
+    <div>
+      <button>Sign Up Now</button>
+      <div>
+        <a href="#">Follow Us</a>
+        <a href="#">
+          <img src={FacebookImg} alt="facebook" />
+        </a>
+        <a href="#">
+          <img src={TwitterImg} alt="twitter" />
+        </a>
+        <a href="#">
+          <img src={YoutubeImg} alt="youtube" />
+        </a>
+        <a href="#">
+          <img src={PinterestImg} alt="pinterest" />
+        </a>
+        <a href="#">
+          <img src={PeriscopeImg} alt="periscope" />
+        </a>
+      </div>
+    </div>
+  );
 }
 export default FooterLink;
